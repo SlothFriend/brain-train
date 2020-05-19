4cb82c7028acb8611851db4bca8dfa5f0c3b0787
David Clark <davidclark314@gmail.com>
2020-05-18 23:50:26 -0600

    Started a folder for example trainings to record progress.

---
+++ trainings/AND_from_known_structure.ts
@@ -0,0 +1,21 @@
+import ANDBrain from '../brains/ANDBrain'
+import Trainer from '../classes/Trainer'
+
+const trainer = new Trainer(ANDBrain.scoreFn, [
+  new ANDBrain(),
+  new ANDBrain(),
+  new ANDBrain(),
+])
+
+let bestScore = 0
+let cycle = 0
+while (bestScore < 4) {
+  console.log('Cycle:', cycle++)
+
+  trainer.mutateBrains(20)
+  const scores = trainer.cull()
+  bestScore = Math.max(bestScore, ...scores)
+}
+
+console.log('END RESULTS')
+trainer.printAllBrainDetails()
